0b98081416ca8c1d893a322e3660c2aa0efd9136
Tommaso Boschi <boschi.tommaso@gmail.com>
2021-03-04 09:24:53 -0800

    pep8 naming convention

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Coding challenge by SG
 
-The solutions to problems 1-6 are structured as the `coding_challenge` package.
+The solutions to problems 1-6 are structured as the `codingchallenge` package.
 Each problem has its own module, named `problemX`, where `X` is the problem number.
 The method `solve` is available in each module and it returns the correct answer.
 
--- test.py
+++ test.py
@@ -2,13 +2,13 @@
 
 import timeit
 
-from coding_challenge import problem1 as p1
-from coding_challenge import problem2 as p2
-from coding_challenge import problem3 as p3
-from coding_challenge import problem4 as p4
-from coding_challenge import problem5 as p5
-from coding_challenge import problem6 as p6
-from coding_challenge import problem7 as p7
+from codingchallenge import problem1 as p1
+from codingchallenge import problem2 as p2
+from codingchallenge import problem3 as p3
+from codingchallenge import problem4 as p4
+from codingchallenge import problem5 as p5
+from codingchallenge import problem6 as p6
+from codingchallenge import problem7 as p7
 
 challenge = {'problem 1' : p1,
              'problem 2' : p2,
